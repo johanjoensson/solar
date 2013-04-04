5c2e8c85c35f5ff7cba0a0cf26bb1e3716282261
Gustav Svensk <grulfen3@gmail.com>
2013-04-04 18:09:23 +0200

    Skybox funkar!

--- src/body.cpp
+++ src/body.cpp
@@ -1,11 +1,20 @@
 #include "body.h"
 
+Body::Body(const char* model, const char *texture) : Object(model, texture)
+{
+    mass = 1;
+    spin_x = 0;
+    spin_y = 0;
+    spin_z = 0;
+    radius = 1;
+}
+
 Body::Body(const char* model) : Object(model)
 {
     mass = 1;
     spin_x = 0;
     spin_y = 0;
-    spin_z= 0;
+    spin_z = 0;
     radius = 1;
 }
 
@@ -18,7 +27,12 @@ Body::Body()
     radius = 1;
 }
 
-    
+void Body::draw(int program)
+{
+    glUniformMatrix4fv(glGetUniformLocation(program, "mdl_matrix"), 1, GL_TRUE, matrix.m);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    DrawModel(m, program, "in_position", "in_normal", "in_tex_coord");
+}
 
 void Body::update(float dt)
 {
